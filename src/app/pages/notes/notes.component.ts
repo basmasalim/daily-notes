df5838c1aedd6984d0e369fe70bd687b756bf5f4
basmasalim <basmasalim030@gmail.com>
2025-05-22 10:00:05 +0300

    drag & drop

--- src/app/pages/notes/notes.component.ts
+++ src/app/pages/notes/notes.component.ts
@@ -1,14 +1,63 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { ModalComponent } from '../../shared/components/modal/modal.component';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+import { NotesData } from '../../core/interfaces/notes-data';
 
 @Component({
   selector: 'app-notes',
-  imports: [ModalComponent],
+  imports: [ModalComponent, CdkDropList, CdkDrag],
   templateUrl: './notes.component.html',
-  styleUrl: './notes.component.css'
+  styleUrl: './notes.component.css',
 })
 export class NotesComponent {
   modalVisible = signal(false);
+  notes = signal<NotesData[]>([]);
+  inProgress = signal<NotesData[]>([]);
+  done = signal<NotesData[]>([]);
+
+  deleteNotes(noteId: number) {
+    this.notes.set([]);
+    this.inProgress.set([]);
+    this.done.set([]);
+  }
+
+  editNotes(noteId: number, note: NotesData) {
+    // this.notes.update((notes) => {
+    //   const index = notes.findIndex((note) => note._id === noteId);
+    //   notes[index] = note;
+    //   return notes;
+    // });
+  }
+
+  drop(event: CdkDragDrop<WritableSignal<NotesData[]>>) {
+    const prevData = event.previousContainer.data();
+    const currData = event.container.data();
+
+    if (event.previousContainer === event.container) {
+      const updated = [...currData];
+      moveItemInArray(updated, event.previousIndex, event.currentIndex);
+      event.container.data.set(updated);
+    } else {
+      const prevUpdated = [...prevData];
+      const currUpdated = [...currData];
+
+      transferArrayItem(
+        prevUpdated,
+        currUpdated,
+        event.previousIndex,
+        event.currentIndex
+      );
+
+      event.previousContainer.data.set(prevUpdated);
+      event.container.data.set(currUpdated);
+    }
+  }
 
   openModal() {
     this.modalVisible.set(true);
@@ -17,5 +66,4 @@ export class NotesComponent {
   closeModal() {
     this.modalVisible.set(false);
   }
-
 }
--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,17 +1,47 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnDestroy, OnInit, Output, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Input() isVisible: boolean = false;
   @Output() onClose = new EventEmitter<void>();
+
+  modalForm!: FormGroup;
+  isEditEnabled = signal(false);
+
+  private readonly formBuilder = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.modalForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: '',
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.modalForm.value);
+  }
+
+  addNote() {
+    console.log('addNote');
+  }
+  updateNote() { }
 
   handleOverlayClick() {
     this.onClose.emit();
   }
-
 }
